a6bd3288f1cb1a786fb831cdbb7a48cf7b87b5f2
iamrahulmeena <rahulmeenaoffical@gmail.com>
2024-05-05 00:29:48 +0530

    Stack and Heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -75,3 +75,17 @@ console.log(typeof anotherId);
 //        Arrays  =>  object
 //        Function  =>  function
 //        Object  =>  object
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Stack and Heap memory in js
+
+// datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+// JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
