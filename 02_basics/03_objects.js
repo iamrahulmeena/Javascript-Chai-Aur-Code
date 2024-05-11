ecd89cf230634c200bd590f0f1e65571486c49ac
iamrahulmeena <rahulmeenaoffical@gmail.com>
2024-05-11 06:41:29 +0530

    objects in js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,2 +1,46 @@
-// singleton
-// Object.create
+//we have 2 types to declear objects -> 1. object literals            2. constructor
+
+// singleton     -> ye apne tarh ka ek hi object hota hai (can create using constructor)
+// Object.create  //constractor methot ->esse hamsa singleton banta hai
+
+
+
+// object literals -> esme singleton nhi banta hai.
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Rahul",
+    "full name": "Rahul Meena",
+    [mySym]: "mykey1", //squre brackets rewuired for a symbol otherwise its work like a string
+    age: 20,
+    location: "Jaipur",
+    email: "Rahul@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+
+console.log(JsUser.email)          
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
+
+
+JsUser.email = "Rahul@chatgpt.com"  //changes will be accepted
+// Object.freeze(JsUser)   //object freeze means now we cant change the info
+JsUser.email = "Rahul@microsoft.com" //due to above freeze these changes will not implement
+console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
